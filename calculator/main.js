01e46005fcacc2073e3fb5e8a29dcd4d9c7df8cd
yogeshdcool <dyogesh.official@gmail.com>
2023-06-13 15:16:22 +0530

    added calculator

---
+++ calculator/main.js
@@ -0,0 +1,119 @@
+const buttons = document.querySelectorAll("#buttons>*");
+const screen = document.querySelector("#screen > span");
+
+const operators = "+-*/%=";
+
+click_sound = new Audio("./audio/keypress.mp3");
+enter_sound = new Audio("./audio/enter.mp3");
+
+document.addEventListener("keydown", (e) => {
+    console.log(e.key);
+
+    switch (e.key) {
+        case "Enter":
+            main("=");
+            break;
+        case "Backspace":
+            main("C");
+            break;
+        case "Escape":
+            main("AC");
+    }
+
+    if (operators.includes(e.key) || !isNaN(e.key)) {
+        main(e.key);
+    }
+});
+
+buttons.forEach((element) => {
+    element.addEventListener("click", (event) => {
+        main(event.target.textContent);
+    });
+});
+
+function main(value) {
+    console.log(value);
+    if (value === "=") {
+        enter_sound.currentTime = 0;
+        enter_sound.play();
+    } else {
+        click_sound.currentTime = 0;
+        click_sound.play();
+    }
+
+    if (screen.textContent === "NaN" || screen.textContent === "Infinity") {
+        screen.textContent = "";
+    }
+
+    if (value === "AC") {
+        screen.textContent = "";
+        return;
+    }
+    if (value === "C") {
+        screen.textContent = screen.textContent.slice(0, -1);
+        return;
+    }
+    if (value === ".") {
+        if (screen.textContent.includes(".")) {
+            return;
+        }
+    }
+    if (operators.includes(value)) {
+        console.log(screen.textContent.charAt(screen.textContent.length - 1));
+        if (
+            operators.includes(
+                screen.textContent.charAt(screen.textContent.length - 1)
+            )
+        ) {
+            if (value != "=") {
+                screen.textContent = screen.textContent.slice(0, -1) + value;
+            }
+            return;
+        } else {
+            operate(
+                screen.textContent.match(
+                    /([.\d]+(?:e\+\d+)?)([+\-*\/%])?(\d+)?/
+                )
+            );
+
+            if (value === "=") {
+                return;
+            }
+        }
+    }
+
+    screen.textContent += value;
+}
+
+function operate(matches) {
+    if (matches[3] !== undefined) {
+        console.log("not single");
+        var x = Number(matches[1]);
+        var y = Number(matches[3]);
+        var op = matches[2];
+        console.log(matches);
+
+        switch (op) {
+            case "+":
+                console.log(+x + +y);
+                screen.textContent = +x + +y;
+                break;
+            case "-":
+                console.log(x - y);
+                screen.textContent = x - y;
+                break;
+            case "*":
+                console.log(x * y);
+                screen.textContent = x * y;
+                break;
+            case "/":
+                console.log(x / y);
+                screen.textContent = x / y;
+                break;
+            case "%":
+                console.log(x % y);
+                screen.textContent = x % y;
+                break;
+        }
+    }
+}
